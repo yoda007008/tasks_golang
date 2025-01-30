0c4ee88277919140fbdcb74fa19cfdcaeeb6db66
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-01-30 16:39:44 +0300

    task second complite

--- task2/main.go
+++ task2/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
+// кубы чисел в диапазоне a и b
+
 func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+	c := b + 1
+	for i := range c {
+		if i >= a {
+			fmt.Println(i, "cube =", i*i*i)
+		}
+	}
 
 }

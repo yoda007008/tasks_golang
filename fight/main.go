f00f6a96db368f08d82c34c42e0a4968b40d388f
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-02-01 11:12:41 +0300

    fix task4

--- fight/main.go
+++ fight/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var index int
+	fmt.Println("Введите число от 0 до 10, чтобы попытать удачу: ")
+	fmt.Scan(&index)
+	// y := [10]string{"@", "@", "@", "@", "X", "X", "X", "X", "@", "@"}
+	/*
+		for range y {
+			fmt.Println("Введите число от 0 до 10, чтобы попытать удачу: ")
+			if y[index] == "X" {
+				fmt.Println("Попал!")
+			} else if y[index] == "@" {
+				fmt.Println("Мимо!")
+			}
+		}
+	*/
 }

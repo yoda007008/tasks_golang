62b975e76bde894eef87caee0a02a8dbba8a58f3
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-02-01 14:28:33 +0300

    sea battle complite

--- fight/main.go
+++ fight/main.go
@@ -1,20 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-	var index int
-	fmt.Println("Введите число от 0 до 10, чтобы попытать удачу: ")
-	fmt.Scan(&index)
-	// y := [10]string{"@", "@", "@", "@", "X", "X", "X", "X", "@", "@"}
-	/*
-		for range y {
-			fmt.Println("Введите число от 0 до 10, чтобы попытать удачу: ")
-			if y[index] == "X" {
-				fmt.Println("Попал!")
-			} else if y[index] == "@" {
-				fmt.Println("Мимо!")
+const (
+	boardSize = 10
+	shipSize  = 4
+)
+
+func SeaBattle() {
+	my_board := [boardSize]string{"S", "S", "S", "S", "/", "/", "/", "/", "/", "/"}
+
+	for {
+		var cord int
+		fmt.Print("Введите координату от 1 до 10: ")
+		fmt.Scan(&cord)
+
+		cord--
+		if cord < 0 || cord >= boardSize {
+			fmt.Println("Некорректные координаты для корабля")
+			continue
+		}
+
+		if my_board[cord] == "S" {
+			my_board[cord] = "X"
+			fmt.Println("Попал!")
+		}
+
+		shipDown := true
+
+		for _, cell := range my_board {
+			if cell == "S" {
+				shipDown = false
+				break
 			}
 		}
-	*/
+
+		if shipDown {
+			fmt.Println("Выигрыш!")
+			for _, cell := range my_board {
+				fmt.Printf("%s", cell)
+			}
+			fmt.Println()
+			break
+		} else if my_board[cord] == "/" {
+			my_board[cord] = "@"
+			fmt.Println("Промах, попробуй еще раз")
+		}
+
+	}
+}
+
+func main() {
+	SeaBattle()
 }

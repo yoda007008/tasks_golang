094b7b0b8cc489f40b384145c4c6387f3f1647e9
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-02-07 12:50:57 +0300

    new function AddProduct

--- warehouse/main.go
+++ warehouse/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Product struct { // структура товара на складе
 	ID       int
 	Name     string
@@ -13,6 +15,18 @@ type Storage struct { // содержание склада
 type Warehouse interface {
 	AddProduct(product Product) error
 	UpdateProduct(productID int, quantity int)
+}
+
+func (st *Storage) AddProduct(product Product) error {
+	if st.Products == nil {
+		st.Products = make(map[int]Product)
+		fmt.Println("Продукт успешно добавлен")
+	}
+	if _, ok := st.Products[product.ID]; ok {
+		return fmt.Errorf("Товар с ID %d уже существует на складе", product.ID)
+	}
+	st.Products[product.ID] = product
+	return nil
 }
 
 func main() {

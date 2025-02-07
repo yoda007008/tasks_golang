182f02e4d0a2a9f9135309fe44ebbe8fc47fd42a
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-02-07 11:25:48 +0300

    practice oop with bank system

---
+++ practice_oop/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type Account struct {
+	owner   string
+	balance float64
+}
+
+func NewAccount(owner string) *Account {
+	return &Account{owner: owner}
+}
+
+func (a *Account) SetBalance(quantity float64) error {
+	if quantity < 0 {
+		fmt.Println("Отрицательный баланс")
+	}
+	a.balance = quantity
+	return nil
+}
+
+func (a *Account) GetBalance() float64 {
+	return a.balance
+}
+
+func (a *Account) Deposit(quantity float64) error {
+	if quantity < 0 {
+		fmt.Println("Отрицательный или нулевой депозит")
+	}
+	a.balance += quantity // зачисление средств
+	return nil
+}
+
+func (a *Account) Withdraw(quantity float64) error {
+	if quantity < 0 {
+		fmt.Println("Отрицательный депозит")
+	}
+	if a.balance-quantity < 0 {
+		fmt.Println("Отрицательный депозит")
+	}
+	a.balance -= quantity // снятие средств с депозита
+	return nil
+}
+
+func main() {
+
+}

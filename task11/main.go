ac6a32af8243f21868cbf61e25ef77eb4086fa4e
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-02-10 17:46:48 +0300

    task 11 complite

--- task11/main.go
+++ task11/main.go
@@ -1,1 +1,21 @@
-package task11
+package main
+
+import "fmt"
+
+const (
+	rowCount = 3
+	colCount = 3
+)
+
+var Matrix [rowCount][colCount]int
+
+// матрица, заполненная целыми числами
+func main() {
+	for row := 0; row < rowCount; row++ {
+		for col := 0; col < colCount; col++ {
+			Matrix[row][col] = row + col
+			fmt.Print(Matrix[row][col], " ")
+		}
+		fmt.Println("")
+	}
+}
---
+++ task12/main.go
@@ -0,0 +1,7 @@
+package main
+
+const MatrixSize = 3
+
+func main() {
+
+}

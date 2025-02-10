41b3091891f956dfc052f2be2533398059395d7e
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-02-10 21:21:01 +0300

    task12 complite

--- task12/main.go
+++ task12/main.go
@@ -1,7 +1,43 @@
 package main
 
+import "fmt"
+
 const MatrixSize = 3
 
-func main() {
+var Matrix [MatrixSize][MatrixSize]int
 
+func main() {
+	fmt.Println("Заполните матрицу", MatrixSize, "x", MatrixSize, "числами построчно")
+
+	for rowNum := 0; rowNum < MatrixSize; rowNum++ {
+		for colNum := 0; colNum < MatrixSize; colNum++ {
+			fmt.Print(rowNum+1, "-я строка ", colNum+1, "-й столбец: ")
+			fmt.Scan(&Matrix[rowNum][colNum])
+		}
+	}
+
+	// Вывод матрицы на экран + подсчет суммы
+	fmt.Println("Введенная матрица:")
+	rowSum := [MatrixSize]int{}
+	colSum := [MatrixSize]int{}
+
+	for rowNum := 0; rowNum < MatrixSize; rowNum++ {
+		fmt.Print(" | ")
+		for colNum := 0; colNum < MatrixSize; colNum++ {
+			fmt.Printf("%4d  ", Matrix[rowNum][colNum])
+			rowSum[rowNum] += Matrix[rowNum][colNum]
+			colSum[colNum] += Matrix[rowNum][colNum]
+		}
+		fmt.Println("|")
+	}
+
+	fmt.Println("Сумма по строкам:")
+	for i := 0; i < MatrixSize; i++ {
+		fmt.Printf("%d-я строка: %d\n", i+1, rowSum[i])
+	}
+
+	fmt.Println("\nСумма по столбцам!")
+	for i := 0; i < MatrixSize; i++ {
+		fmt.Printf("%d-й столбец: %d\n", i+1, colSum[i])
+	}
 }

09bc3d81aadbc3f2c24639a3bb9c6694b7a24ee1
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-03-01 20:44:34 +0300

    gazstation new refactor

--- gazstation/main.go
+++ gazstation/main.go
@@ -12,19 +12,14 @@ type Car struct {
 
 type GazStation struct {
 	amountFuel int
+	gazVolume  int
 }
 
-//func (c *Car) AcceptCar() { // данная функция будет принимать машину
-//	fmt.Println("Введи имя машины: ")
-//	fmt.Scan(&c.name)
-//	fmt.Println("Добро пожаловать на заправочную станцию,", c.name)
-//	fmt.Println("Введи текущее количество топлива: ")
-//	fmt.Scan(&c.volume)
-//	fmt.Println("Введи максимальное значение бака: ")
-//	fmt.Scan(&c.maxVolume)
-//}
-
 func NewCar(name string, volume int, maxVolume int) *Car { // конструктор для новой машины
+	if volume < 0 || maxVolume < 0 {
+		fmt.Println("Ошибка: введены отрицательные значения.")
+	}
+
 	return &Car{
 		name:      name,
 		volume:    volume,
@@ -32,34 +27,33 @@ func NewCar(name string, volume int, maxVolume int) *Car { // конструкт
 	}
 }
 
-func (c *Car) Refuel(g *GazStation) { // обновленная функция для заправки автомобиля
-	if c.volume < 0 || c.maxVolume < 0 {
-		fmt.Println("Ошибка: введены отрицательные значения.")
-		return
-	}
-
-	if c.volume == c.maxVolume {
-		fmt.Println("Ваша машина уже с полным баком.")
-	} else if c.volume < c.maxVolume {
-		neededFuel := c.maxVolume - c.volume
-		fmt.Printf("Заправляю топлива на %d литров.\n", neededFuel)
-		c.volume = c.maxVolume
-		fmt.Println("Ваша машина успешно заправлена.")
-		g.amountFuel -= neededFuel
-		fmt.Printf("Топливо, которое осталось %d литров", g.amountFuel)
+func (c *Car) Refuel(amount int) { // добавляет топливо в автомобиль
+	if c.volume+amount > c.maxVolume {
+		c.volume = c.maxVolume // обновление до максимального объема, если сумма amount + c.volume превышает макс. объем
+		fmt.Println("Бак заполнен")
 	} else {
-		fmt.Printf("false\n")
+		c.volume += amount
 	}
 }
 
+func (g *GazStation) GetFuel(amount int) bool { // проверяет достаточно ли топлива на заправке для автомобиля
+	if g.amountFuel < amount {
+		return false
+	}
+	g.amountFuel -= amount
+	return true
+}
+
 func main() {
-	fuel := GazStation{amountFuel: 10}
-	firstCar := NewCar("Kia", -2, -2)
-	secondCar := NewCar("Ford", 2, 3)
-	thirdCar := NewCar("Kia", 3, 2)
-	fmt.Printf("Объем топлива %d литров\n", fuel)
-	firstCar.Refuel(&GazStation{amountFuel: 10})
-	secondCar.Refuel(&GazStation{amountFuel: 10})
-	fmt.Println("")
-	thirdCar.Refuel(&GazStation{amountFuel: 10})
+	station := &GazStation{amountFuel: 100}
+	car := NewCar("Kia", 30, 50)
+	amount := 25
+	if station.GetFuel(amount) {
+		car.Refuel(amount)
+		fmt.Printf("Автомобилю %s удалось заправиться\n", car.name)
+	} else {
+		fmt.Println("На заправке недостаточно топлива")
+	}
+	fmt.Printf("Топливо в автомобиле %s: %d/%d\n", car.name, car.volume, car.maxVolume)
+	fmt.Printf("Топливо на заправке: %d", station.amountFuel)
 }

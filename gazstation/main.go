bcf75ba35bda2a21082c51816736e3d66f1d7e2a
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-03-01 12:19:35 +0300

    gazstation refactor

--- gazstation/main.go
+++ gazstation/main.go
@@ -11,19 +11,28 @@ type Car struct {
 }
 
 type GazStation struct {
+	amountFuel int
 }
 
-func (c *Car) AcceptCar() { // данная функция будет принимать машину
-	fmt.Println("Введи имя машины: ")
-	fmt.Scan(&c.name)
-	fmt.Println("Добро пожаловать на заправочную станцию,", c.name)
-	fmt.Println("Введи текущее количество топлива: ")
-	fmt.Scan(&c.volume)
-	fmt.Println("Введи максимальное значение бака: ")
-	fmt.Scan(&c.maxVolume)
+//func (c *Car) AcceptCar() { // данная функция будет принимать машину
+//	fmt.Println("Введи имя машины: ")
+//	fmt.Scan(&c.name)
+//	fmt.Println("Добро пожаловать на заправочную станцию,", c.name)
+//	fmt.Println("Введи текущее количество топлива: ")
+//	fmt.Scan(&c.volume)
+//	fmt.Println("Введи максимальное значение бака: ")
+//	fmt.Scan(&c.maxVolume)
+//}
+
+func NewCar(name string, volume int, maxVolume int) *Car { // конструктор для новой машины
+	return &Car{
+		name:      name,
+		volume:    volume,
+		maxVolume: maxVolume,
+	}
 }
 
-func (g *GazStation) RefuelCar(c *Car) { // данная функция будет заправлять машину
+func (c *Car) Refuel(g *GazStation) { // обновленная функция для заправки автомобиля
 	if c.volume < 0 || c.maxVolume < 0 {
 		fmt.Println("Ошибка: введены отрицательные значения.")
 		return
@@ -36,17 +45,21 @@ func (g *GazStation) RefuelCar(c *Car) { // данная функция буде
 		fmt.Printf("Заправляю топлива на %d литров.\n", neededFuel)
 		c.volume = c.maxVolume
 		fmt.Println("Ваша машина успешно заправлена.")
+		g.amountFuel -= neededFuel
+		fmt.Printf("Топливо, которое осталось %d литров", g.amountFuel)
 	} else {
-		fmt.Println("Ошибка: текущий объем топлива превышает максимальный объем бака.")
+		fmt.Printf("false\n")
 	}
 }
 
 func main() {
-	for {
-		myCar := Car{}
-		myCar.AcceptCar()
-
-		station := GazStation{}
-		station.RefuelCar(&myCar)
-	}
+	fuel := GazStation{amountFuel: 10}
+	firstCar := NewCar("Kia", -2, -2)
+	secondCar := NewCar("Ford", 2, 3)
+	thirdCar := NewCar("Kia", 3, 2)
+	fmt.Printf("Объем топлива %d литров\n", fuel)
+	firstCar.Refuel(&GazStation{amountFuel: 10})
+	secondCar.Refuel(&GazStation{amountFuel: 10})
+	fmt.Println("")
+	thirdCar.Refuel(&GazStation{amountFuel: 10})
 }

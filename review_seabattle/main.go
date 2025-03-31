627a16d4bda47fbcaa71c0aefff164c4bff10cb1
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-03-31 21:39:00 +0300

    review seabattle

---
+++ review_seabattle/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	game := NewGame(2)
+
+	shipSizes := []int{4, 3, 3, 2, 2, 2, 1, 1, 1, 1}
+
+	for _, player := range game.(*GameImpl).players {
+		p := player.(*PlayerImpl)
+
+		for _, size := range shipSizes {
+			err := p.board.PlaceShipRandom(size)
+			if err != nil {
+				fmt.Printf("Ошибка при размещении корабля размером %d: %v\n", size, err)
+			}
+		}
+
+		fmt.Printf("\nПоле игрока %d после размещения кораблей:\n", p.id)
+		p.board.PrintField()
+	}
+
+	round := 1
+	for !game.IsEnded() {
+		fmt.Printf("\n--- Раунд %d ---\n", round)
+		game.Round()
+		round++
+	}
+
+	var winnerID int
+	for _, player := range game.(*GameImpl).players {
+		if player.StatusPlayer() {
+			winnerID = player.(*PlayerImpl).id
+			break
+		}
+	}
+
+	fmt.Printf("\nИгра окончена! Победил игрок %d\n", winnerID)
+}
---
+++ review_seabattle/realization/ship.go
@@ -0,0 +1,57 @@
+package realization
+
+type ShipStatus int
+
+const (
+	Alive ShipStatus = iota // цел
+	Hurt                    // ранен
+	Dead                    // мёртв
+)
+
+type Ship interface {
+	GetStatus() ShipStatus
+	HandleShoot(x int, y int) bool
+}
+
+type StandardShipImpl struct {
+	decks       []ShipStatus
+	x           int // todo getX, координата начальной точки корабля
+	y           int // todo getY, координата начальной точки корабля
+	orientation int // todo iota
+	status      ShipStatus
+	size        int
+}
+
+func (s StandardShipImpl) GetStatus() ShipStatus {
+	deadCount := 0
+	for _, status := range s.decks {
+		if status == Dead {
+			deadCount++
+		}
+	}
+	if deadCount == s.size {
+		return Dead
+	} else if deadCount == 0 {
+		return Alive
+	}
+	return Hurt
+}
+
+func (s *StandardShipImpl) HandleShoot(x, y int) bool {
+	if s.orientation == gorizontal {
+		if y != s.y || x < s.x || x >= s.x+s.size {
+			return false
+		}
+		s.decks[x-s.x] = Dead
+	} else {
+		if x != s.x || y < s.y || y >= s.y+s.size {
+			return false
+		}
+		s.decks[y-s.y] = Dead
+	}
+	return true
+}
+
+func NewShip() Ship {
+	return &StandardShipImpl{}
+}

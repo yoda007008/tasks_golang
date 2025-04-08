cde6f7812d71ce4858d4932ed11d6d13660e3451
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-04-08 21:23:10 +0300

    full game complite

--- text_game/main.go
+++ text_game/main.go
@@ -6,14 +6,16 @@ import (
 )
 
 type Game struct {
-	rooms     map[string]Room
-	player    Player
-	doorState bool // состояние двери, false закрыто, true открыто
+	rooms        map[string]Room
+	player       Player
+	doorState    bool
+	kitchenFirst bool // Флаг первого посещения кухни
 }
 
 type Room struct {
 	description string
-	items       []string
+	tableItems  []string // предметы на столе
+	chairItems  []string // предметы на стуле
 	exits       []string
 }
 
@@ -23,105 +25,263 @@ type Player struct {
 	backPack  bool // наличие рюкзака
 }
 
-func initGame() *Game { // инициализация игры
+func initGame() *Game {
 	return &Game{
 		rooms: map[string]Room{
 			"кухня": {
 				description: "ты находишься на кухне, надо собрать рюкзак и идти в универ",
-				items:       []string{"чай"},
+				tableItems:  []string{"чай"},
+				chairItems:  []string{},
 				exits:       []string{"коридор"},
 			},
 			"комната": {
-				description: "ты находишься в комнате",
-				items:       []string{"ключи", "конспекты", "рюкзак"},
+				description: "ты в своей комнате",
+				tableItems:  []string{"ключи", "конспекты"},
+				chairItems:  []string{"рюкзак"},
 				exits:       []string{"коридор"},
 			},
 			"коридор": {
-				description: "ты находишься в коридоре, тут ничего интересеного",
+				description: "ничего интересного",
+				tableItems:  []string{},
+				chairItems:  []string{},
 				exits:       []string{"кухня", "комната", "улица"},
 			},
 			"улица": {
-				description: "ты находишься на улице",
+				description: "на улице весна",
+				tableItems:  []string{},
+				chairItems:  []string{},
 				exits:       []string{"домой"},
 			},
 		},
 		player: Player{
-			state:     "Кухня",
+			state:     "кухня", // должно быть в нижнем регистре
 			inventory: []string{},
 			backPack:  false,
 		},
-		doorState: false,
+		doorState:    false,
+		kitchenFirst: true,
 	}
 }
 
-func lookAround(game *Game) string { // осмотреться
-	// получаем текущую локацию
-	curLocation := game.player.state
-	loc := game.rooms[curLocation]
+func lookAround(game *Game) string {
+	loc := game.rooms[game.player.state]
 
-	var itemsStr, exitsStr string
-
-	if len(loc.items) > 0 {
-		itemsStr = "на столе: " + strings.Join(loc.items, ", ")
+	var itemsParts []string
+	if len(loc.tableItems) > 0 {
+		itemsParts = append(itemsParts, "на столе: "+strings.Join(loc.tableItems, ", "))
+	}
+	if len(loc.chairItems) > 0 {
+		itemsParts = append(itemsParts, "на стуле: "+strings.Join(loc.chairItems, ", "))
 	}
 
-	exitsStr = "можно пройти - " + strings.Join(loc.exits, ", ")
-	return fmt.Sprintf("%s, %s, %s", loc.description, itemsStr, exitsStr)
+	itemsStr := strings.Join(itemsParts, ", ")
+	exitsStr := "можно пройти - " + strings.Join(loc.exits, ", ")
+
+	switch game.player.state {
+	case "кухня":
+		// если есть рюкзак - показываем полное описание с "надо идти в универ"
+		if game.player.backPack {
+			if itemsStr != "" {
+				return fmt.Sprintf("ты находишься на кухне, %s, надо идти в универ. %s",
+					itemsStr, exitsStr)
+			}
+			return fmt.Sprintf("ты находишься на кухне, надо идти в универ. %s",
+				exitsStr)
+		}
+
+		// если это первое посещение кухни
+		if game.kitchenFirst {
+			game.kitchenFirst = false
+			if itemsStr != "" {
+				return fmt.Sprintf("ты находишься на кухне, %s, надо собрать рюкзак и идти в универ. %s",
+					itemsStr, exitsStr)
+			}
+			return fmt.Sprintf("ты находишься на кухне, надо собрать рюкзак и идти в универ. %s",
+				exitsStr)
+		}
+
+		// все остальные случаи - полное описание
+		if itemsStr != "" {
+			return fmt.Sprintf("ты находишься на кухне, %s, надо собрать рюкзак и идти в универ. %s",
+				itemsStr, exitsStr)
+		}
+		return fmt.Sprintf("ты находишься на кухне, надо собрать рюкзак и идти в универ. %s",
+			exitsStr)
+
+	case "комната":
+		if len(loc.tableItems) == 0 && len(loc.chairItems) == 0 {
+			return fmt.Sprintf("пустая комната. %s", exitsStr)
+		}
+		if itemsStr != "" {
+			return fmt.Sprintf("%s. %s", itemsStr, exitsStr)
+		}
+		return fmt.Sprintf("%s. %s", loc.description, exitsStr)
+
+	default:
+		if itemsStr != "" {
+			return fmt.Sprintf("%s, %s. %s", loc.description, itemsStr, exitsStr)
+		}
+		return fmt.Sprintf("%s. %s", loc.description, exitsStr)
+	}
 }
 
-func goTo(destination string, game *Game) string { // перемещение игрока
-	curLocation := game.player.state
-	loc := game.rooms[curLocation]
+func goTo(destination string, game *Game) string {
+	loc := game.rooms[game.player.state]
 
 	for _, exit := range loc.exits {
 		if exit == destination {
-			if destination == "улица" && game.doorState != false {
-				return "Дверь закрыта"
+			if destination == "улица" && !game.doorState {
+				return "дверь закрыта"
 			}
 			game.player.state = destination
-			return game.rooms[destination].description + "можно пройти - " + strings.Join(game.rooms[destination].exits, ", ")
+
+			// При входе в комнату - только описание
+			if destination == "комната" {
+				return fmt.Sprintf("%s. можно пройти - коридор",
+					game.rooms[destination].description)
+			}
+
+			// При возвращении на кухню - всегда краткое описание
+			if destination == "кухня" {
+				return fmt.Sprintf("кухня, ничего интересного. можно пройти - коридор")
+			}
+
+			return lookAround(game)
 		}
 	}
-	return "Нет пути в " + destination
+	return "нет пути в " + destination
 }
 
-func takeItem(item string, game *Game) { // данный метод удаляет предмет с локации и перемещает его в инвентарь к игроку
-	curLocation := game.player.state
-	loc := game.rooms[curLocation]
+func takeItem(item string, game *Game) string {
+	loc := game.rooms[game.player.state]
 
-	for i, locItem := range loc.items {
+	// Проверяем предметы на столе
+	for i, locItem := range loc.tableItems {
 		if locItem == item {
-			if !game.player.backPack && item == "рюкзак" {
+			if !game.player.backPack && item != "рюкзак" {
 				return "некуда класть"
 			}
-			game.rooms[game.player.state].items = append(loc.items[:i], loc.items[i+1:]...)
 
+			loc.tableItems = append(loc.tableItems[:i], loc.tableItems[i+1:]...)
+			game.rooms[game.player.state] = loc
 			game.player.inventory = append(game.player.inventory, item)
-
-			return "предмет добавлен в инвентарь" + item
+			return "предмет добавлен в инвентарь: " + item
 		}
 	}
+
+	// Проверяем предметы на стуле
+	for i, locItem := range loc.chairItems {
+		if locItem == item {
+			if !game.player.backPack && item != "рюкзак" {
+				return "некуда класть"
+			}
+
+			loc.chairItems = append(loc.chairItems[:i], loc.chairItems[i+1:]...)
+			game.rooms[game.player.state] = loc
+			game.player.inventory = append(game.player.inventory, item)
+			return "предмет добавлен в инвентарь: " + item
+		}
+	}
+
+	return "нет такого"
 }
 
-func wearItem(item string, game *Game) string { // данный метод проверяет наличие рюкзака и ищет его в текущей локации
+func wearItem(item string, game *Game) string {
 	if item != "рюкзак" {
 		return "можно надеть только рюкзак"
 	}
 
-	for i, lockItem := range game.rooms[game.player.state].items {
-		if lockItem == "рюкзак" {
-			game.rooms[game.player.state].items = append(game.rooms[game.player.state].items[:i], game.rooms[game.player.state].items[i+1:]...)
+	loc := game.rooms[game.player.state]
+
+	// Проверяем рюкзак на стуле
+	for i, locItem := range loc.chairItems {
+		if locItem == "рюкзак" {
+			loc.chairItems = append(loc.chairItems[:i], loc.chairItems[i+1:]...)
+			game.rooms[game.player.state] = loc
 			game.player.backPack = true
-			return "вы надели рюкзак"
+			return "вы надели: рюкзак"
 		}
 	}
-	return "нет такого предмета"
+
+	// Проверяем рюкзак на столе
+	for i, locItem := range loc.tableItems {
+		if locItem == "рюкзак" {
+			loc.tableItems = append(loc.tableItems[:i], loc.tableItems[i+1:]...)
+			game.rooms[game.player.state] = loc
+			game.player.backPack = true
+			return "вы надели: рюкзак"
+		}
+	}
+
+	return "нет такого"
 }
 
-func handleCommand(command string, game *Game) {
+func useItem(item, target string, game *Game) string {
+	hasItem := false
+	for _, invItem := range game.player.inventory {
+		if invItem == item {
+			hasItem = true
+			break
+		}
+	}
 
+	if !hasItem {
+		return "нет предмета в инвентаре - " + item
+	}
+
+	if item == "ключи" && target == "дверь" && game.player.state == "коридор" {
+		game.doorState = true
+		return "дверь открыта"
+	}
+
+	return "не к чему применить"
+}
+
+func handleCommand(command string, game *Game) string {
+	parts := strings.Split(command, " ")
+	switch parts[0] {
+	case "осмотреться":
+		return lookAround(game)
+	case "идти":
+		if len(parts) < 2 {
+			return "укажите место"
+		}
+		return goTo(parts[1], game)
+	case "взять":
+		if len(parts) < 2 {
+			return "укажите предмет"
+		}
+		return takeItem(parts[1], game)
+	case "надеть":
+		if len(parts) < 2 {
+			return "укажите предмет"
+		}
+		return wearItem(parts[1], game)
+	case "применить":
+		if len(parts) < 3 {
+			return "укажите предмет и объект"
+		}
+		return useItem(parts[1], parts[2], game)
+	default:
+		return "неизвестная команда"
+	}
 }
 
 func main() {
+	game := initGame()
+	fmt.Println("Добро пожаловать в текстовую игру!")
+	fmt.Println("Доступные команды: осмотреться, идти [место], взять [предмет], надеть [предмет], применить [предмет] [объект]")
 
+	for {
+		fmt.Print("> ")
+		var input string
+		fmt.Scanln(&input)
+
+		if input == "выход" {
+			break
+		}
+
+		response := handleCommand(input, game)
+		fmt.Println(response)
+	}
 }

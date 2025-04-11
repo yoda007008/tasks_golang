229a887a23c786469e07a801c910ba42e9e2454f
Kirill Petrovskiy <masteryoda01010101@gmail.com>
2025-04-11 20:20:06 +0300

    fourth task

--- tasks_gorutin/task4/main.go
+++ tasks_gorutin/task4/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+var (
+	counter int
+	wg      sync.WaitGroup
+	mu      sync.Mutex
+)
+
+func IncrementCount() {
+	defer wg.Done()
+	mu.Lock()
+	counter += 1
+	mu.Unlock()
 }
+
+func main() {
+	count := 10 // количество горутин
+	wg.Add(count)
+	for i := 0; i < count; i++ {
+		go IncrementCount()
+	}
+	wg.Wait()
+	fmt.Println(count)
+}
+
+//Потокобезопасный инкремент - Mutex.
+//Задача: Напишите программу, где 10 горутин инкрементируют один счётчик, защищая его sync.Mutex.
